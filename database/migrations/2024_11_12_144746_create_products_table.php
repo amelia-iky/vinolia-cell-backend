a4772876ce9020f29b4b458fb60b7777479352fd
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-13 20:47:19 +0800

    fix count decimal in price

--- database/migrations/2024_11_12_144746_create_products_table.php
+++ database/migrations/2024_11_12_144746_create_products_table.php
@@ -10,11 +10,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('brand');
+            $table->string('brand'); 
             $table->string('name');
-            $table->decimal('price', 8, 2);
-            $table->text('description');
-            $table->file('images');
+            $table->decimal('price', 12, 2)->default(0.00);
+            $table->text('description'); 
+            // $table->file('images');
             $table->timestamps();
         });
     }
